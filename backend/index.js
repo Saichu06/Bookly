b0eac5ea5c28f2b5dfc4129ca433c8007546f50d
Saichu <assaicharan06@gmail.com>
2026-06-23 12:16:56 +0530

    React Initialized - router dom added

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors()) 
 
 app.get('/', (req, res) => {
     return res.status(200).send("Welcome to book store!");
